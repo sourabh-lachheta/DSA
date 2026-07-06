8c79499d3f84cf11b7bfbb29c31058faecdfcccd
sourabh <sourabhlachheta@gmail.com>
2026-07-06 21:45:58 +0530

    solved Leetcode no.69 : square root

--- Code/src/com/Problems/Sqrt_69.java
+++ Code/src/com/Problems/Sqrt_69.java
@@ -5,15 +5,30 @@
 package com.Problems;
 
 public class Sqrt_69 {
-    class Solution{
+    static class Solution{
         public static int mysqrt(int x){
-             x = x^(1/2);
-            return x;
+            int start = 0;
+            int end = x;
+            int ans = 0;
+            while(start <= end){
+                int mid = start + (end - start)/2;
+                long s = (long) mid * mid; // to avoid overflow of int
+                if(s == x){
+                    return mid;
+                }
+                if(s > x){
+                    end = mid -1;
+                }else{
+                    start = mid + 1;
+                    ans = mid;
+                }
+            }
+            return ans;
         }
     }
 
     public static void main(String[] args){
-        int x = 4;
+        int x = 8;
         System.out.println(Solution.mysqrt(x));
     }
 }
