822c1e93fb5110010f9f21261aff76ca33200f18
sourabh <sourabhlachheta@gmail.com>
2026-07-08 20:53:17 +0530

    solved leetcode 367 : perfect square

---
+++ Code/src/com/Problems/PerfectSquare_367.java
@@ -0,0 +1,30 @@
+
+
+// LeetCode 367:  Valid Perfect Square
+
+package com.Problems;
+public class PerfectSquare_367 {
+
+    public static boolean isPerfectSquare(int num) {
+        int start = 0;
+        int end = num;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(mid * mid == num){
+                return true;
+            }
+            if(mid*mid > num){
+                end = mid - 1;
+            }else{
+                start = mid +1;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+
+        int num = 9;
+        System.out.println(isPerfectSquare(num));
+
+    }
+}
