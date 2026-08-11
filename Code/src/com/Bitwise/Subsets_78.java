944a2722471c6a0f2f6b20212c8ab54939c882d4
sourabh <sourabhlachheta@gmail.com>
2026-08-11 17:14:02 +0530

    adding gcd

---
+++ Code/src/com/Bitwise/Subsets_78.java
@@ -0,0 +1,14 @@
+package com.Bitwise;
+import java.util.*;
+
+public class Subsets_78 {
+   // private static List<List<Integer>> subsets(int[] nums) {
+
+
+   // }
+    public static void main(String[] args){
+        String s = "1+1";
+        int num1 = Integer.parseInt(s);
+        System.out.println(num1);
+    }
+}
---
+++ Code/src/com/maths/Prime.java
@@ -0,0 +1,24 @@
+package com.maths;
+
+
+public class Prime {
+
+    private static boolean isPrime(int n){
+        if(n <=1){
+            return false;
+        }
+
+        int c = 2;
+        while(c * c <= n){
+            if(n%c == 0){
+                return false;
+            }
+            c++;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        int n = 7;
+        System.out.println(isPrime(n));
+    }
+}
